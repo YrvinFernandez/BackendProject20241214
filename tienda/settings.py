3567b71d0996e6adffc7cb166058ae9c61f119d8
Yrvin Fernandez <yrvin.fernandez@tecsup.edu.pe>
2024-12-10 21:36:42 -0500

    secret key

--- tienda/settings.py
+++ tienda/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5&74m)%y01orjxr+ajnly&w(@jp%by5p(#r=t9p+&&e0&0n^2x'
+SECRET_KEY = ('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
