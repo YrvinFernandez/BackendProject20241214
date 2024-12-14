f7f68ee07fe2fb6656b9b22952d57ad874cda04b
Yrvin Fernandez <yrvin.fernandez@tecsup.edu.pe>
2024-12-13 21:44:34 -0500

    test

--- tienda/settings.py
+++ tienda/settings.py
@@ -11,11 +11,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config, AutoConfig
+from decouple import config
 from datetime import timedelta
 import os
 
-config = AutoConfig(search_path='/ruta/al/directorio/con/el/env')
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
